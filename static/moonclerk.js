d1b58a5c12ba7316ab336fc167395734cc2354e9
r00t-101-LoL <adhd101js@gmail.com>
2021-04-21 20:47:33 +0000

    Initial commit

---
+++ static/moonclerk.js
@@ -0,0 +1,16 @@
+var mc3lg4rogyyklg
+;(function (d, t) {
+  var s = d.createElement(t),
+    opts = { checkoutToken: "3lg4rogyyklg", width: "100%" }
+  s.src = "https://d2l7e0y6ygya2s.cloudfront.net/assets/embed.js"
+  s.onload = s.onreadystatechange = function () {
+    var rs = this.readyState
+    if (rs) if (rs != "complete") if (rs != "loaded") return
+    try {
+      mc3lg4rogyyklg = new MoonclerkEmbed(opts)
+      mc3lg4rogyyklg.display()
+    } catch (e) {}
+  }
+  var scr = d.getElementsByTagName(t)[0]
+  scr.parentNode.insertBefore(s, scr)
+})(document, "script")
